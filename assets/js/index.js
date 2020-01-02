871b71072430ab43b97a052751ea13ac28e7e9c4
jeaninet <46909170+jeaninet@users.noreply.github.com>
2020-01-01 21:24:39 -0800

    personal website

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+let menuOpen = false;
+document.getElementById("menu-lines").onclick = doMenuThings;
+
+function toggleClose() {
+  let lineOne = document.getElementById("line-1");
+  let lineTwo = document.getElementById("line-2");
+  let lineThree = document.getElementById("line-3");
+  if (!menuOpen) {
+    lineOne.style.animation = "lineOne 0.5s";
+    lineOne.style.animationFillMode = "forwards";
+    lineTwo.style.animation = "lineTwo 0.5s";
+    lineTwo.style.animationFillMode = "forwards";
+    lineThree.style.animation = "lineThree 0.5s";
+    lineThree.style.animationFillMode = "forwards";
+  } else {
+    lineOne.style.animation = "lineOneBack 0.5s";
+    lineOne.style.animationFillMode = "forwards";
+    lineTwo.style.animation = "lineTwoBack 0.5s";
+    lineTwo.style.animationFillMode = "forwards";
+    lineThree.style.animation = "lineThreeBack 0.5s";
+    lineThree.style.animationFillMode = "forwards";
+  }
+}
+function toggleMenu() {
+  let menu = document.getElementById("menu");
+  if (!menuOpen) {
+    menu.style.animation = "slideIn 0.5s";
+    menu.style.animationFillMode = "forwards";
+    menuOpen = true;
+  } else {
+    menu.style.animation = "slideOut 0.5s";
+    menu.style.animationFillMode = "forwards";
+    menuOpen = false;
+  }
+}
+
+function doMenuThings() {
+  toggleClose();
+  toggleMenu();
+}
