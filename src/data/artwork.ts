9ae66da00456fde48b532dc919ecd84bad96b409
jeaninetso <jtso3388@gmail.com>
2026-07-28 16:04:14 -0700

    Reorder Art & Design pieces
    New order: Observe My Ways, KingPin Donuts, The Tso Family,
    Julian's Room, First-Gen Mentorship Introduction, Eagle Academy
    Christmas Party, UC Berkeley Welcome Week.

--- src/data/artwork.ts
+++ src/data/artwork.ts
@@ -6,19 +6,9 @@ export type ArtworkPiece = {
 
 export const artwork: ArtworkPiece[] = [
   {
-    title: 'UC Berkeley Welcome Week',
-    image: '/images/uc-berkeley-welcome-week.png',
-    text: 'Flyers designed for UC Berkeley Welcome Week events. Tools: Canva.',
-  },
-  {
-    title: 'Eagle Academy Christmas Party (Cambodia)',
-    image: '/images/eagle-academy-christmas-party.png',
-    text: 'Flyer designed for a Christmas party during my year mentoring students in Cambodia. Tools: Canva.',
-  },
-  {
-    title: 'First-Gen Mentorship Introduction',
-    image: '/images/first-gen-mentorship-intro.png',
-    text: 'Flyer introducing a first-generation student mentorship initiative with Acts 2 Campus Network. Tools: Canva.',
+    title: 'Observe My Ways',
+    image: '/images/proverbs.png',
+    text: 'Inspired by Proverbs 23:26, "My son, give me your heart, and let your eyes observe my ways." Intended to portray the closeness of scrutiny that humans need to have of their own lives if they are to call themselves examples for others. Tools: Adobe Illustrator.',
   },
   {
     title: 'KingPin Donuts',
@@ -26,9 +16,9 @@ export const artwork: ArtworkPiece[] = [
     text: 'Illustrated a variety of donut flavors for the menu of a website redesign (see Projects). Flavor varieties are from KingPin Donuts. Tools: Adobe Illustrator.',
   },
   {
-    title: 'Observe My Ways',
-    image: '/images/proverbs.png',
-    text: 'Inspired by Proverbs 23:26, "My son, give me your heart, and let your eyes observe my ways." Intended to portray the closeness of scrutiny that humans need to have of their own lives if they are to call themselves examples for others. Tools: Adobe Illustrator.',
+    title: 'The Tso Family',
+    image: '/images/tso-family.png',
+    text: 'LEGO versions of my family members, gifted to my dad for his birthday. Tools: Adobe Illustrator.',
   },
   {
     title: "Julian's Room",
@@ -36,8 +26,18 @@ export const artwork: ArtworkPiece[] = [
     text: 'Drew my brother\'s bedroom as a Christmas present. Colors inspired by the music video for Penomeco\'s "Coco Bottle," animated by Studio Pivote. Tools: Adobe Illustrator.',
   },
   {
-    title: 'The Tso Family',
-    image: '/images/tso-family.png',
-    text: 'LEGO versions of my family members, gifted to my dad for his birthday. Tools: Adobe Illustrator.',
+    title: 'First-Gen Mentorship Introduction',
+    image: '/images/first-gen-mentorship-intro.png',
+    text: 'Flyer introducing a first-generation student mentorship initiative with Acts 2 Campus Network. Tools: Canva.',
+  },
+  {
+    title: 'Eagle Academy Christmas Party (Cambodia)',
+    image: '/images/eagle-academy-christmas-party.png',
+    text: 'Flyer designed for a Christmas party during my year mentoring students in Cambodia. Tools: Canva.',
+  },
+  {
+    title: 'UC Berkeley Welcome Week',
+    image: '/images/uc-berkeley-welcome-week.png',
+    text: 'Flyers designed for UC Berkeley Welcome Week events. Tools: Canva.',
   },
 ]
